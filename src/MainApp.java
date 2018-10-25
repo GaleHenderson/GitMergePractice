afb11046a9f3b1f4bb96d64b5d720e3d168c1ef2
gale <gale.a.chapman@gmail.com>
2018-10-25 13:44:15 -0400

    added a line of code

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,8 +3,11 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("My name is Gale.");
 System.out.println("hello world");
 System.out.println("Goodbye, cruel world!");
+
 		
 	}
 
